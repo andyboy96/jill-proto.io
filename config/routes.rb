c3f20c0611806e19b1ba80c5d0d4e8e2d367c8ed
CSSI 2015 <CSSI@CSSIs-Air.hsd1.il.comcast.net>
2015-08-01 11:26:16 -0500

    new formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_for :views
+
+  resources :securities
   get 'welcome/homepage'
 
   # The priority is based upon order of creation: first created -> highest priority.
